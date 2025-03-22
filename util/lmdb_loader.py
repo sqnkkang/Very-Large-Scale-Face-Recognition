9b2fac9988058e5e8c918804167e51c6188b75d6
fangqing <96216206+sqnkkang@users.noreply.github.com>
2025-03-22 15:04:53 +0800

    Update lmdb_loader.py

--- util/lmdb_loader.py
+++ util/lmdb_loader.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import lmdb
-from data.datum_def_pb2 import Datum
+from data import Datum
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import RandomSampler
