af10a96c662f52ba2c012060cf1154994373fb8d
fangqing <96216206+sqnkkang@users.noreply.github.com>
2025-03-22 22:21:09 +0800

    Update ffc.py

--- ffc.py
+++ ffc.py
@@ -7,18 +7,6 @@ from resnet_def import create_net
 import os
 import logging as logger
 
-'''
-日志文件，记录训练过程中的相关信息
-'''
-logger.basicConfig(level=logger.INFO,
-                   format='%(levelname)s %(asctime)s %(filename)s: %(lineno)d] %(message)s',
-                   datefmt='%Y-%m-%d %H:%M:%S')
-'''
-每次张量操作都会计算梯度，这个装饰器代表这个合并的张量操作不记录梯度，虽然我只用一个 GPU 用不到这个函数
-'''
-@torch.no_grad()
-def concat_all_gather(tensor): return tensor
-
 class FFC(Module):
     def __init__(self, net_type, feat_dim, queue_size=7409, scale=32.0, loss_type='AM', margin=0.4, momentum=0.99,
                  neg_margin=0.25, pretrained_model_path=None, num_class=None):
@@ -30,6 +18,7 @@ class FFC(Module):
         '''
         创建两个网络也就是论文里面提到的 p 网络和 g 网络，分别进行更新 DCP 和进行梯度下降损失的计算
         '''
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.probe_net = create_net(net_type, feat_dim=feat_dim, fp16=True)
         self.gallery_net = create_net(net_type, feat_dim=feat_dim, fp16=True)
 
@@ -45,7 +34,9 @@ class FFC(Module):
         self.scale = scale
         self.margin = margin
         self.loss_type = loss_type
-
+        '''
+        初始化我们的 lru，下面的 queue_position_sict 是一个字典能记录队列里面每个位置的使用情况
+        '''
         self.lru = LRU(queue_size)
         self.queue_position_dict = {}
         for i in range(queue_size):
@@ -62,11 +53,15 @@ class FFC(Module):
         for param_p, param_g in zip(self.probe_net.parameters(), self.gallery_net.parameters()):
             param_g.data.copy_(param_p.data)  # initialize
             param_g.requires_grad = False  # not update by gradient
-
+    '''
+    下面构建了常见的三个带边界的损失函数，开始是时候先构建正负样本的索引
+    '''
     def add_margin(self, cos_theta, label):
         outlier_label = torch.where(label == -1)[0]
         pos_label_idx = torch.where(label != -1)[0]
-        
+        '''
+        正样本的数量大于 0 个，则计算一下正样本的损失
+        '''
         if self.loss_type == 'AM':
             if pos_label_idx.numel() > 0:
                 pos_cos_theta = cos_theta[pos_label_idx]
@@ -138,24 +133,31 @@ class FFC(Module):
         """
         for param_p, param_g in zip(self.probe_net.parameters(), self.gallery_net.parameters()):
             param_g.data = param_g.data * self.m + param_p.data * (1. - self.m)
-
+    '''
+    因为我们本来的 batch 就是可以理解为一式两份的，前一份用来更新我们的 DCP，另一份用来更新模型里面的参数
+    需要注意的是，他的论文原文表述的意思和代码有不相符的地方
+    接下来判断现在的这个 batch 里面是不是有些人脸没出现在 lru 里面，默认调用 __contains__ 函数
+    要是有新的人脸没有出现在 lru 里面，将当前位置的 rows 设置为 0，得到当前的 DCP 里面的存储的位置 存储到 cols 里面 这个位置标记为出现在了 DCP 里面
+    否则的话，直接 get 得到当前的位置，双队列机制能更好的存储更多的特征的信息，避免单一队列的局限性
+    '''
     def forward_impl(self, p_data, g_data, probe_label, gallery_label):
+        '''
+        先将 p_data 放进 p 网络里面进行前向传播，对于 p 网络的计算的时候我们不需要梯度下降，因为这个前向传播只是为了更新我们的 DCP
+        '''
         p = self.probe_net(p_data)
-        with torch.no_grad():  # no gradient to gallery
-            g_single_rank = self.gallery_net(g_data)
-            g = concat_all_gather(g_single_rank)
-            g_label_all_tensor = concat_all_gather(gallery_label)
-            g_label_list = g_label_all_tensor.tolist()
+        with torch.no_grad():
+            g = self.gallery_net(g_data)
+            g_label_list = gallery_label.tolist()
 
         rows = []
         cols = []
         # old_state = {}
         ones_idx = set([])
-        for i, gl in enumerate(g_label_list):  # [0, 0, 0, 0,]
+        for i, gl in enumerate(g_label_list):
             if gl not in self.lru:
                 idx = self.lru.get(gl)
                 rows.append(0)
-                cols.append(idx)  #
+                cols.append(idx)
                 self.queue_position_dict[idx] = 1
             else:
                 idx = self.lru.get(gl)
@@ -168,12 +170,18 @@ class FFC(Module):
         c = torch.LongTensor(cols).cuda(g.device)
         with torch.no_grad():
             self.queue[r, c] = g
+        '''
+        看一下 p 标签是不是再 DCP 里面，计算一下 p 特征和队列里面的特征的余弦相似度，主要步骤为
+        先和队列里面的第一行进行计算余弦相似度
+        然后计算 fake_labels 这个东西队列里面有的话就会返回在队列里面的位置，否则就会返回 -1
+        mask 通过动态选择队列，增强特征的多样性和模型的表达能力，感觉没什么逻辑
+        '''
         fake_labels = []
         probe_label_list = probe_label.tolist()
         for pl in probe_label_list:
             fake_labels.append(self.lru.view(pl))
-        label = torch.LongTensor(fake_labels).cuda(p.device)
 
+        label = torch.LongTensor(fake_labels).cuda(p.device)
         cos_theta1 = F.linear(p, self.queue[0]) 
         mask_idx = torch.LongTensor(list(ones_idx))
         self.mask[mask_idx, 0] = 1
@@ -188,10 +196,8 @@ class FFC(Module):
         p = self.probe_net(p_data)
         with torch.no_grad():  # no gradient to gallery
             self._momentum_update_gallery() # update the gallery net
-            g_single_rank = self.gallery_net(g_data)
-            g = concat_all_gather(g_single_rank)
-            g_label_all_tensor = concat_all_gather(gallery_label)
-            g_label_list = g_label_all_tensor.tolist()
+            g = self.gallery_net(g_data)
+            g_label_list = gallery_label.tolist()
         rows = []
         cols = []
         old_state = {}
@@ -240,7 +246,6 @@ class FFC(Module):
         self.mask[mask_idx, 0] = 0
         self.lru.rollback_steps(steps)
         return loss
-
 
     def forward(self, x, y, x_label, y_label):
         loss2 = self.forward_impl_rollback(x, y, x_label, y_label)
